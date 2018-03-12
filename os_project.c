8c86afddf00b3f59e0101d791728cfd19be31ff3
Vikas Shukla <vikasshukhla@gmail.com>
2018-03-13 01:03:07 +0530

    Operating system project

--- os_project.c
+++ os_project.c
@@ -64,3 +64,48 @@ main()
 	{
 		array2[i]=array1[i+1];
 	}
+  printf("\n\n\t\t\tGANTT CHART OF THE PROCESS ACCORDING TO GIVEN PRIORITY\n\n\t\t\t");
+	int m=n-1;
+	int t=0;
+	if(0==pro[0].arrivaltime)
+	{
+	printf(" 0 ");
+	}
+	else
+	{
+		printf(" 0  %d",pro[0].arrivaltime);
+		t=t+pro[0].arrivaltime;
+	}
+	t=t+pro[0].bursttime;
+	printf("  P%d  %d",pro[0].processid,t);
+	completiontime[1]=t;
+
+    do
+	{
+		int tmp=0,j,u;
+		for( j=0;j<m;j++)
+		{
+			for( u=0;u<n;u++)
+			{
+				if(array2[j]==pro[u].processid)
+				{
+					if(pro[u].arrivaltime<=t)
+					{
+						array3[tmp++]=pro[u].processid;
+					}
+				}
+			}
+		}
+		int tmp1=tmp+1;
+		double array4[tmp1];
+		int tmp2=4,k,o;
+		for( k=0;k<tmp1-1;k++)
+		{
+			for( o=0;o<n;o++)
+			{
+				if(array3[k]==pro[o].processid)
+				{
+					array4[k]=1+(double)(t-pro[o].arrivaltime)/pro[o].bursttime;
+				}
+			}
+		}
